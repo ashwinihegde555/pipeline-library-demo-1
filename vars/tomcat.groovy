6d02edc201e7e128212419eee369d336327bae79
ashwinihegde555 <56389774+ashwinihegde555@users.noreply.github.com>
2020-11-09 11:10:14 +0530

    Update tomcat.groovy

--- vars/tomcat.groovy
+++ vars/tomcat.groovy
@@ -1,1 +1,4 @@
-def foo;
+def startTomcat(){
+  echo "*************Starting tomcat***********************"
+  bat F:\\DEVOPS\\apache-tomcat-8.5.58-windows-x64\\apache-tomcat-8.5.58\\bin startup.bat
+}
